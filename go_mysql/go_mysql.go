1b42e713712438fca2b3527887a23c6f0516e55d
superman-wrdh <13419655360@163.com>
2018-11-05 23:52:32 +0800

    # mysql

--- go_mysql/go_mysql.go
+++ go_mysql/go_mysql.go
@@ -13,7 +13,6 @@ func checkErr(err error) {
 func main() {
 	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/mytest?charset=utf8")
 	checkErr(err)
-	//插入数据
 
 	fmt.Println("insert data info into table, userinfo")
 
