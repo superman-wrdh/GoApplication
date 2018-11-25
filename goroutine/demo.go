c39fee1e5ae872f9203ffd79178e2919ae914f13
superman-wrdh <13419655360@163.com>
2018-11-26 00:15:11 +0800

    goroutine

--- goroutine/demo.go
+++ goroutine/demo.go
@@ -5,21 +5,31 @@ import (
 	"fmt"
 )
 
-var message = make(chan string)
+/**
+FIFO
+ */
 
-func sample() {
-	message <- "hello world"
+func sample(message chan string) {
+	message <- "hello goroutine1"
+	message <- "hello goroutine2"
+	message <- "hello goroutine3"
+	message <- "hello goroutine4"
 }
 
-func sample2() {
+func sample2(message chan string) {
 	time.Sleep(time.Second)
 	str := <-message
 	str = str + " I am go goroutine"
 	message <- str
+	close(message) //最后一次往里面写
 }
 func main() {
-	go sample()
-	go sample2()
-	time.Sleep(3 * time.Second)
-	fmt.Println(<-message)
+	var message = make(chan string, 3)
+	go sample(message)
+	go sample2(message)
+	time.Sleep(time.Second)
+	for str := range message {
+		fmt.Println(str)
+	}
+	fmt.Println("hello world")
 }
