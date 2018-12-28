8fa59ff697357727123c7a33208b6a2d955cc835
superman-wrdh <13419655360@163.com>
2018-12-28 23:55:36 +0800

    # BF

---
+++ algorithm/PrintTree.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+/**
+*
+ * 题目：从上往下打印出二叉树的每个节点，同层节点从左至右打印
+ * 广度优先层次遍历，
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	tree := CreateTree()
+	PrintBF(&tree)
+}
+
+func PrintBF(root *TreeNode) {
+	if root != nil {
+		fmt.Printf(" %v ", root.Val)
+	}
+	if root.Left != nil {
+		PrintBF(root.Left)
+		//nodeList = append(nodeList, node.Left)
+	}
+	if root.Right != nil {
+		PrintBF(root.Right)
+		//nodeList = append(nodeList, node.Right)
+	}
+
+}
+
+func CreateTree() TreeNode {
+	var treeIs = `
+		 3
+	   / \
+	  9    20
+	     /   \
+	    15     7
+    `
+	fmt.Println("create a tree like\n", treeIs)
+	root := TreeNode{
+		3,
+		&TreeNode{9, nil, nil},
+		&TreeNode{20,
+			&TreeNode{15, nil, nil},
+			&TreeNode{7, nil, nil}}}
+	return root
+}
