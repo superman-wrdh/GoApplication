46c51805ebf8782bc2a1bff301b4b2fbb232f317
superman-wrdh <13419655360@163.com>
2019-01-06 01:02:04 +0800

    # go redis

---
+++ go_redis/demo.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/go-redis/redis"
+	"fmt"
+)
+
+func InitClient() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+	return client
+	// Output: PONG <nil>
+}
+
+func ExampleClient() {
+	client := InitClient()
+	//key string, value interface{}
+	err := client.Set("key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := client.Get("key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := client.Get("key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
+	// Output: key value
+	// key2 does not exist
+}
