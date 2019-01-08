28ca47c90e8fa4971ffc80c68bfb7e034142c9df
superman-wrdh <13419655360@163.com>
2019-01-08 23:57:09 +0800

    # file MD5

---
+++ io/file_md5.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("G:\\GOPATH\\src\\GoApplication\\io\\file_md5.go")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	h := md5.New()
+	if _, err := io.Copy(h, f); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%x", h.Sum(nil))
+}
