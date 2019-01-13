1f6f2a8a30edb2810a3031862fd96fba7bec0989
superman-wrdh <13419655360@163.com>
2019-01-13 22:01:16 +0800

    # time operation

---
+++ go_time/LocationTime.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// China doesn't have daylight saving. It uses a fixed 8 hour offset from UTC.
+	secondsEastOfUTC := int((8 * time.Hour).Seconds())
+	beijing := time.FixedZone("Beijing Time", secondsEastOfUTC)
+
+	// If the system has a timezone database present, it's possible to load a location
+	// from that, e.g.:
+	//    newYork, err := time.LoadLocation("America/New_York")
+
+	// Creating a time requires a location. Common locations are time.Local and time.UTC.
+	timeInUTC := time.Date(2009, 1, 1, 12, 0, 0, 0, time.UTC)
+	sameTimeInBeijing := time.Date(2009, 1, 1, 20, 0, 0, 0, beijing)
+
+	// Although the UTC clock time is 1200 and the Beijing clock time is 2000, Beijing is
+	// 8 hours ahead so the two dates actually represent the same instant.
+	timesAreEqual := timeInUTC.Equal(sameTimeInBeijing)
+	fmt.Println(timesAreEqual)
+
+}
