74717638a860fdc47cf82cd5dae8f2caada1c61a
superman-wrdh <13419655360@163.com>
2019-01-21 23:28:38 +0800

    # go data structures # mergeTwoLists

---
+++ algorithm/DeleteNodeinALinkedList.go
@@ -0,0 +1,37 @@
+package algorithm
+
+/**
+Merge Two Sorted Lists
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+
+*/
+
+/**
+* Definition for singly-linked list.
+* type ListNode struct {
+*     Val int
+*     Next *ListNode
+* }
+
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/771/
+*/
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists(l1, l2.Next)
+		return l2
+	}
+}
